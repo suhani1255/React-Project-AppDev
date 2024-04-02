2867640772af1e808e4436574d1fbf5f9cce1ad1
Suhani Patel <suhani1255@gmail.com>
2024-04-01 23:13:34 -0400

    pdf

--- react project/src/App.jsx
+++ react project/src/App.jsx
@@ -2,13 +2,58 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Pdf from '../Documents/resPDF.pdf';
+
+function AboutMe({props}){
+  return <h4>I am a {props.major} major graduating in 2027!</h4>;
+
+}
+
+function AboutMeP() {
+  const year = "2027";
+  const major = "Computer Science";
+  return (
+    <>
+    <h1>About Me</h1>
+    <AboutMe major = {major}/>
+    </>
+  );
+}
+
+
+// Navbar component
+function NavBar() {
+  return (
+    <nav>
+      <div>
+        <h1 className="logo">Suhani Patel</h1>
+      </div>
+      <ul className="nav-links">
+        <li>
+          Home
+        </li>
+        <li>
+          Resume
+        </li>
+        <li>
+          Hobbies
+        </li>
+        <li>
+          Contact
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar />
       <div>
+        <a href = {Pdf}>Resume</a>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,8 +61,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>About Me</h1>
+      <div className="about">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
